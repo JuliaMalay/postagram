2963ec0d74a23730ba7f07d543787a1a6f773787
Julia Malay <malayjulia14@gmail.com>
2021-08-17 15:01:23 +0700

    created UI component Loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useMemo, useState} from 'react';
+import PostService from './API/PostService';
 import {usePosts} from './components/hooks/usePosts';
 import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 import Button from './components/UI/button/Button';
+import Loader from './components/UI/Loader/Loader';
 import Modal from './components/UI/modal/Modal';
 import './styles/App.css';
 const axios = require('axios');
@@ -13,6 +15,7 @@ function App() {
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
   useEffect(() => {
     fetchPosts();
@@ -24,10 +27,10 @@ function App() {
   };
 
   async function fetchPosts() {
-    const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    );
-    setPosts(response.data);
+    setIsPostsLoading(true);
+    const posts = await PostService.getAll();
+    setPosts(posts);
+    setIsPostsLoading(false);
   }
 
   const removePost = (post) => {
@@ -36,7 +39,6 @@ function App() {
 
   return (
     <div className="App">
-      <Button onClick={fetchPosts}>Get data</Button>
       <Button
         onClick={() => {
           setModal(true);
@@ -49,12 +51,19 @@ function App() {
       </Modal>
       <hr style={{margin: '10px 0'}} />
       <PostFilter filter={filter} setFilter={setFilter} />
-
-      <PostList
-        posts={sortedAndSearchedPosts}
-        title="Список постов"
-        remove={removePost}
-      />
+      {isPostsLoading ? (
+        <div
+          style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          posts={sortedAndSearchedPosts}
+          title="Список постов"
+          remove={removePost}
+        />
+      )}
     </div>
   );
 }
