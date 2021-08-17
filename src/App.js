81aa73e800bc86690cafb462368b3d4863d2fcf2
Julia Malay <malayjulia14@gmail.com>
2021-08-17 14:12:16 +0700

    added user hook usePosts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useMemo, useState} from 'react';
+import {usePosts} from './components/hooks/usePosts';
 import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
@@ -26,22 +27,7 @@ function App() {
   ]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
-
-  // useMemo первый аргумент - результат вычислений, второй - массив зависимостей
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query)
-    );
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
